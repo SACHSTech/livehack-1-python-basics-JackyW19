98c10930f46e254c594acf56404f3de171e4a0dc
JackyW29 <jacky.wang23@ycdsbk12.ca>
2020-12-07 18:06:33 +0000

    completed problem 1

--- problem1.py
+++ problem1.py
@@ -1,1 +1,17 @@
+Name:		problem1.py
+Purpose:	compute temperature in celsius to fahrenheit and compute it to the user.
 
+Author:	Wang.J
+
+Created:	date in 07/12/2020
+
+print ("**** Welcome to the celsius to fahrenheit ****")
+
+# get the temperature in celsius from the UserWarning
+celsius_temperature = float(input("Enter the temperature in celsius: "))
+
+# compute the temperature in celsius to fahrenheit
+fahrenheit = float(celsius_temperature*(9/5) + 32)
+
+# output the converted fahrenheit temperature
+print ("The temperature in fahrenheit is: " + str(fahrenheit))
