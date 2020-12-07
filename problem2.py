73d7367527e48566df706cd72c49d9593bcfa4dc
JackyW29 <jacky.wang23@ycdsbk12.ca>
2020-12-07 18:49:53 +0000

    problem 2

--- problem2.py
+++ problem2.py
@@ -17,7 +17,7 @@ chicken_pieces = int(input("Enter the number of popeyes chicken pieces: "))
 
 # compute the number of popeyes chicken pieces each student and the teacher will get
 chicken_students = int(round(chicken_pieces / students))
-Mr_Fabroa = int(round(students % chicken_pieces))
+Mr_Fabroa = int(round(chicken_pieces % students))
 
 # output the number of popeyes chicken pieces each student will get and the number of popeyes chicken pieces the teacher will get
 print ("The number of popeyes chicken pieces each student will get is " + str(chicken_students))
